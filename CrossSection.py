b32b5a4dd3f209aeb05ed41b665c2b5732147285
Preston Wilson <prestonawilson@gmail.com>
2018-01-20 20:46:43 -0500

    automated to evaluate full directories now

--- CrossSection.py
+++ CrossSection.py
@@ -1,104 +1,111 @@
+#!/usr/bin/env python
+
+""" Creates and saves plots of vertical cross sections with matplotlib.
+"""
+
+import glob
+import os
+import fnmatch
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
-import cartopy.crs as crs
-import cartopy.feature as cfeature
 from netCDF4 import Dataset
-
 from wrf import (getvar, to_np, vertcross, smooth2d, CoordPair, GeoBounds, get_cartopy,
-                 latlon_coords, cartopy_xlim, cartopy_ylim)
+                 latlon_coords)
 
-# Open the NetCDF file
-ncfile = Dataset("wrfout_d04_2013-09-11_00_00_00.nc")
+___author___ = "Preston Wilson, modified from example code on http://wrf-python.readthedocs.io"
 
-# Get the WRF variables
-slp = getvar(ncfile, "slp")
-smooth_slp = smooth2d(slp, 3)
-ctt = getvar(ncfile, "ctt")
-z = getvar(ncfile, "z")
-dbz = getvar(ncfile, "dbz")
-Z = 10**(dbz/10.)
-wspd = getvar(ncfile, "wspd_wdir", units="kt")[0,:]
+dir_list = raw_input("Enter directory to be evaluated: ")  # example: wrf\* for all files in wrf directory on Windows OS
+new_dir_list = dir_list.split('\\')
+if not os.path.exists('UpdatedPlots/'):
+                os.makedirs('UpdatedPlots/')
+for wrffile in glob.iglob(dir_list):
+    if fnmatch.fnmatch(wrffile, '[wrf]*'):
+        print "Evaluating file " + wrffile
+        ncfile = Dataset(wrffile)
 
-print slp.shape
-print dbz.shape
-print ctt.shape
+        # Get the WRF variables
+        slp = getvar(ncfile, "slp")
+        smooth_slp = smooth2d(slp, 3)
+        ctt = getvar(ncfile, "ctt")
+        z = getvar(ncfile, "z")
+        dbz = getvar(ncfile, "dbz")
+        Z = 10**(dbz/10.)
+        wspd = getvar(ncfile, "wspd_wdir", units="kt")[0,:]
 
-# Set the start point and end point for the cross section
-start_point = CoordPair(x=36.99464, y=-82.37988)
-end_point = CoordPair(x=35.53535, y=-81.0)
+        # Set the start point and end point for the cross section
+        start_point = CoordPair(lat=36.99464, lon=-82.37988)
+        end_point = CoordPair(lat=35.53535, lon=-81.08899)  # For some reason -81.08899 as the y-endpoint gives an error
 
-# Compute the vertical cross-section interpolation.  Also, include the lat/lon
-# points along the cross-section in the metadata by setting latlon to True.
-z_cross = vertcross(Z, z, wrfin=ncfile, start_point=start_point, end_point=end_point,
-                    latlon=True, meta=True)
-wspd_cross = vertcross(wspd, z, wrfin=ncfile, start_point=start_point, end_point=end_point,
-                       latlon=True, meta=True)
-dbz_cross = 10.0 * np.log10(z_cross)
+        # Compute the vertical cross-section interpolation.  Also, include the lat/lon
+        # points along the cross-section in the metadata by setting latlon to True.
+        z_cross = vertcross(Z, z, wrfin=ncfile, start_point=start_point, end_point=end_point,
+                            latlon=True, meta=True)
+        wspd_cross = vertcross(wspd, z, wrfin=ncfile, start_point=start_point, end_point=end_point,
+                               latlon=True, meta=True)
+        dbz_cross = 10.0 * np.log10(z_cross)
 
-# Get the lat/lon points
-lats, lons = latlon_coords(slp)
+        # Get the lat/lon points
+        lats, lons = latlon_coords(slp)
 
-# Get the cartopy projection object
-cart_proj = get_cartopy(slp)
+        # Get the cartopy projection object
+        cart_proj = get_cartopy(slp)
 
-# Create a figure that will have 3 subplots
-fig = plt.figure(figsize=(10,7))
+        # Create a figure that will have 3 subplots
+        fig = plt.figure(figsize=(10,7))
 
-ax_wspd = fig.add_subplot(1,1,1)
+        # Initially had 3 suplots, changed so only wind speed is shown
+        ax_wspd = fig.add_subplot(1,1,1)
+
+        # Make the pressure contours
+        contour_levels = [960, 965, 970, 975, 980, 990]
+
+        # Create the filled cloud top temperature contours
+        contour_levels = [-80.0, -70.0, -60, -50, -40, -30, -20, -10, 0, 10]
+
+        # Crop the domain to the region around the hurricane
+        hur_bounds = GeoBounds(CoordPair(lat=np.amin(to_np(lats)), lon=-85.0),
+                               CoordPair(lat=30.0, lon=-72.0))
+
+        # Make the contour plot for wind speed
+        wspd_contours = ax_wspd.contourf(to_np(wspd_cross), cmap=get_cmap("jet"))
+        # Add the color bar
+        cb_wspd = fig.colorbar(wspd_contours, ax=ax_wspd)
+        cb_wspd.ax.tick_params(labelsize=7)
+
+        # Make the contour plot for dbz
+        levels = [5 + 5*n for n in range(15)]
+
+        # Set the x-ticks to use latitude and longitude labels
+        coord_pairs = to_np(dbz_cross.coords["xy_loc"])
+        x_ticks = np.arange(coord_pairs.shape[0])
+        x_labels = [pair.latlon_str() for pair in to_np(coord_pairs)]
+        ax_wspd.set_xticks(x_ticks[::20])
+        ax_wspd.set_xticklabels([], rotation=45)
+
+        # Set the y-ticks to be height
+        vert_vals = to_np(dbz_cross.coords["vertical"])
+
+        # My quick attempt at making max height 15000; I think it works, but it messes up the y-axis labels and ticks
+        # Commented out for now
+        # vert_vals.flatten()
+        # vert_vals = [i for i in vert_vals if i < 15000]
+
+        v_ticks = np.arange(vert_vals.shape[0])
+        ax_wspd.set_yticks(v_ticks[::20])
+        ax_wspd.set_yticklabels(vert_vals[::20], fontsize=7)
+
+        # Set the x-axis and  y-axis labels
+        ax_wspd.set_ylabel("Height (m)", fontsize=7)
 
 
-# Download and create the states, land, and oceans using cartopy features
-states = cfeature.NaturalEarthFeature(category='cultural', scale='50m', facecolor='none',
-                                      name='admin_1_states_provinces_shp')
-land = cfeature.NaturalEarthFeature(category='physical', name='land', scale='50m',
-                                    facecolor=cfeature.COLORS['land'])
-ocean = cfeature.NaturalEarthFeature(category='physical', name='ocean', scale='50m',
-                                     facecolor=cfeature.COLORS['water'])
+        try:
+            # Add a title
+            ax_wspd.set_title("Cross-Section of Wind Speed (kt)", {"fontsize": 8})
+            fin_name = wrffile.split('\\')
+            plt.savefig('UpdatedPlots/' + fin_name[len(fin_name) - 1] + '_plot.png')
+        except Exception as e:
+            print "Error: " + e
 
-# Make the pressure contours
-contour_levels = [960, 965, 970, 975, 980, 990]
 
-# Create the filled cloud top temperature contours
-contour_levels = [-80.0, -70.0, -60, -50, -40, -30, -20, -10, 0, 10]
-
-# Create the color bar for cloud top temperature
-
-# Draw the oceans, land, and states
-
-# Crop the domain to the region around the hurricane
-hur_bounds = GeoBounds(CoordPair(lat=np.amin(to_np(lats)), lon=-85.0),
-                       CoordPair(lat=30.0, lon=-72.0))
-
-# Make the contour plot for wind speed
-wspd_contours = ax_wspd.contourf(to_np(wspd_cross), cmap=get_cmap("jet"))
-# Add the color bar
-cb_wspd = fig.colorbar(wspd_contours, ax=ax_wspd)
-cb_wspd.ax.tick_params(labelsize=5)
-
-# Make the contour plot for dbz
-levels = [5 + 5*n for n in range(15)]
-
-# Set the x-ticks to use latitude and longitude labels
-coord_pairs = to_np(dbz_cross.coords["xy_loc"])
-x_ticks = np.arange(coord_pairs.shape[0])
-x_labels = [pair.latlon_str() for pair in to_np(coord_pairs)]
-ax_wspd.set_xticks(x_ticks[::20])
-ax_wspd.set_xticklabels([], rotation=45)
-
-# Set the y-ticks to be height
-vert_vals = to_np(dbz_cross.coords["vertical"])
-v_ticks = np.arange(vert_vals.shape[0])
-ax_wspd.set_yticks(v_ticks[::20])
-ax_wspd.set_yticklabels(vert_vals[::20], fontsize=4)
-
-# Set the x-axis and  y-axis labels
-
-ax_wspd.set_ylabel("Height (m)", fontsize=5)
-
-# Add a title
-
-ax_wspd.set_title("Cross-Section of Wind Speed (kt)", {"fontsize" : 7})
-
-plt.show()
 
