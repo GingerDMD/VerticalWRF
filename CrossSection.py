284dec296873603246ec0447aab0d778a29f317c
Preston Wilson <prestonawilson@gmail.com>
2018-03-19 21:47:52 -0400

    Removed second plot, grouped it together

--- CrossSection.py
+++ CrossSection.py
@@ -21,9 +21,9 @@ if not os.path.exists('UpdatedPlots_' + new_dir_list[len(new_dir_list) - 2] + '/
                 os.makedirs('UpdatedPlots_' + new_dir_list[len(new_dir_list) - 2] + '/')
 for wrffile in glob.iglob(dir_list):
     print wrffile
-    # checks for files that contain the string expression; if no error but  not creating plots, consider changing this
-    # necessary check because if non-WRF files are present in the directory, it will try to plot them and break
-    if fnmatch.fnmatch(wrffile, '*[wrf]*'):
+    # Checks for files that contain the string expression; if no error but not creating plots, consider changing this
+    # Necessary check because if non-WRF files are present in the directory, it will try to plot them and break
+    if fnmatch.fnmatch(wrffile, '*[d]*'):
         print "Evaluating file " + wrffile
         ncfile = Dataset(wrffile)
 
@@ -57,20 +57,16 @@ for wrffile in glob.iglob(dir_list):
 
         fig = plt.figure(figsize=(10, 10))
 
-        ax_wspd = fig.add_subplot(2, 1, 1)
-        ax_wspd_zoom = fig.add_subplot(2, 1, 2)
+        ax_wspd = fig.add_subplot(1, 1, 1)
 
         # Make the contour plot for wind speed
         wspd_contours = ax_wspd.contourf(to_np(wspd_cross), cmap=get_cmap("jet"), levels=np.linspace(0, 64, 512))
-        wspd_contours_zoom = ax_wspd_zoom.contourf(to_np(wspd_cross), cmap=get_cmap("hot"), levels=np.linspace(0, 64, 512))
 
         # Add the color bar
         cb_wspd = fig.colorbar(wspd_contours, ax=ax_wspd)
-        cb_wspd_zoom = fig.colorbar(wspd_contours_zoom, ax=ax_wspd_zoom)
 
         # cb_wspd.locator = tick_locator
         cb_wspd.ax.tick_params(labelsize=7)
-        cb_wspd_zoom.ax.tick_params(labelsize=7)
 
         # Make the contour plot for dbz
         levels = [5 + 5*n for n in range(15)]
@@ -82,23 +78,31 @@ for wrffile in glob.iglob(dir_list):
 
         ax_wspd.set_xticks(x_ticks[::30])
         ax_wspd.set_xticklabels(x_labels[::30], fontsize=7, rotation=15)
-        ax_wspd_zoom.set_xticks(x_ticks[::30])
-        ax_wspd_zoom.set_xticklabels(x_labels[::30], fontsize=7, rotation=15)
 
         # Set the y-ticks to be height
         vert_vals = to_np(dbz_cross.coords["vertical"]).astype(int)
         print dbz_cross.coords["vertical"]
 
         v_ticks = np.arange(vert_vals.shape[0])
-        v_ticks_zoom = np.arange(vert_vals.shape[0])
+
         ax_wspd.set_yticks(v_ticks[::10])  # every 10 ax_wspd values serve as tick mark
         ax_wspd.set_yticklabels(vert_vals[::10], fontsize=7)
-        ax_wspd_zoom.set_yticks(v_ticks[::10])
-        ax_wspd_zoom.set_yticklabels(vert_vals[::10], fontsize=7)
 
         # Set the x-axis and  y-axis labels
         ax_wspd.set_ylabel("Height (m)", fontsize=7)
-        ax_wspd_zoom.set_ylabel("Height (m)", fontsize=7)
+
+        # For the second plot, all grouped together
+        # ax_wspd_zoom = fig.add_subplot(2, 1, 2)
+        # wspd_contours_zoom = ax_wspd_zoom.contourf(to_np(wspd_cross),
+        # cmap=get_cmap("hot"), levels=np.linspace(0, 64, 512))
+        # cb_wspd_zoom = fig.colorbar(wspd_contours_zoom, ax=ax_wspd_zoom)
+        # cb_wspd_zoom.ax.tick_params(labelsize=7)
+        # ax_wspd_zoom.set_xticks(x_ticks[::30])
+        # ax_wspd_zoom.set_xticklabels(x_labels[::30], fontsize=7, rotation=15)
+        # v_ticks_zoom = np.arange(vert_vals.shape[0])
+        # ax_wspd_zoom.set_yticks(v_ticks[::10])
+        # ax_wspd_zoom.set_yticklabels(vert_vals[::10], fontsize=7)
+        # ax_wspd_zoom.set_ylabel("Height (m)", fontsize=7)
 
         try:
             # Add a title
@@ -107,7 +111,7 @@ for wrffile in glob.iglob(dir_list):
                               "EP: lat=35.53535, lon=-81.08899", {"fontsize": 10})
             # ax_wspd_zoom.set_title("Zoomed Cross-Section of Lower Elevation Wind Speed", {"fontsize": 8})
             fin_name = wrffile.split('\\')
-            plt.show()
+            # plt.show()
             plt.savefig('UpdatedPlots_' + new_dir_list[len(new_dir_list) - 2] + '/' +
                         fin_name[len(fin_name) - 1] + '_plot.png')
         except Exception as e:
